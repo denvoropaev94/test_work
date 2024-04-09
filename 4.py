e0e11558c3684ee91e0741d6f65cfcb3ec658981
«Denis» <denvoropaev7@gmail.com>
2024-04-09 14:39:53 +0300

    Четвертое

--- 4.py
+++ 4.py
@@ -4,6 +4,6 @@ def sort_strings(strings):
     strings.sort(key=len, reverse=True)  # Сортировка по убыванию
     print("По убыванию: ", strings)
 
-# Пример использования:
+
 strings = ["Ден", "Никитка", "Александр", "Олег", "Сергей"]
 sort_strings(strings)
